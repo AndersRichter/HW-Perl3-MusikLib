b4731445ba53cb9c9a113ce1bf54a3a38f38186c
AndersRichter <andreysav96@rambler.ru>
2016-10-11 23:12:55 +0300

    Create Sortirovchik.pm

---
+++ Sortirovchik.pm
@@ -0,0 +1,122 @@
+package Sortirovchik;
+BEGIN
+{
+	use strict;
+	use warnings;
+	use Exporter();
+	our @ISA = "Exporter";
+	our @EXPORT = qw(&sortirovka_band &sortirovka_year &sortirovka_album &sortirovka_track &sortirovka_form);
+}
+
+sub sortirovka_band
+{
+	my ($len, @arr) = @_;
+	
+	for (my $i=0; $i < ($len-1); $i++)
+	{
+		my $k = 0;
+		for (my $j=0; $j < ($len-$i-1); $j++)
+		{
+			if ($arr[$j][0] gt $arr[$j+1][0]) 
+			{
+				my $b = $arr[$j];
+				$arr[$j] = $arr[$j+1];
+				$arr[$j+1] = $b;
+				$k = 1;
+			}
+		}
+		if($k == 0) {last;}
+	}	
+	return @arr;
+}
+
+sub sortirovka_year
+{
+	my ($len, @arr) = @_;
+	
+	for (my $i=0; $i < ($len-1); $i++)
+	{
+		my $k = 0;
+		for (my $j=0; $j < ($len-$i-1); $j++)
+		{
+			if ($arr[$j][1] > $arr[$j+1][1]) 
+			{
+				my $b = $arr[$j];
+				$arr[$j] = $arr[$j+1];
+				$arr[$j+1] = $b;
+				$k = 1;
+			}
+		}
+		if($k == 0) {last;}
+	}	
+	return @arr;
+}	
+
+sub sortirovka_album
+{
+	my ($len, @arr) = @_;
+	
+	for (my $i=0; $i < ($len-1); $i++)
+	{
+		my $k = 0;
+		for (my $j=0; $j < ($len-$i-1); $j++)
+		{
+			if ($arr[$j][2] gt $arr[$j+1][2]) 
+			{
+				my $b = $arr[$j];
+				$arr[$j] = $arr[$j+1];
+				$arr[$j+1] = $b;
+				$k = 1;
+			}
+		}
+		if($k == 0) {last;}
+	}	
+	return @arr;
+}
+
+sub sortirovka_track
+{
+	my ($len, @arr) = @_;
+	
+	for (my $i=0; $i < ($len-1); $i++)
+	{
+		my $k = 0;
+		for (my $j=0; $j < ($len-$i-1); $j++)
+		{
+			if ($arr[$j][3] gt $arr[$j+1][3]) 
+			{
+				my $b = $arr[$j];
+				$arr[$j] = $arr[$j+1];
+				$arr[$j+1] = $b;
+				$k = 1;
+			}
+		}
+		if($k == 0) {last;}
+	}	
+	return @arr;
+}
+
+sub sortirovka_form
+{
+	my ($len, @arr) = @_;
+	
+	for (my $i=0; $i < ($len-1); $i++)
+	{
+		my $k = 0;
+		for (my $j=0; $j < ($len-$i-1); $j++)
+		{
+			if ($arr[$j][4] gt $arr[$j+1][4]) 
+			{
+				my $b = $arr[$j];
+				$arr[$j] = $arr[$j+1];
+				$arr[$j+1] = $b;
+				$k = 1;
+			}
+		}
+		if($k == 0) {last;}
+	}	
+	return @arr;
+}
+
+return 1;
+END{}
